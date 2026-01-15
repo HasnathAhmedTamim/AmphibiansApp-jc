58f60a858e397bd9b6500d99f77d1a4351ae6387
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-15 14:58:51 +0600

    main activity

--- app/src/main/java/com/example/amphibians/MainActivity.kt
+++ app/src/main/java/com/example/amphibians/MainActivity.kt
@@ -4,44 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.amphibians.ui.theme.AmphibiansTheme
+import com.example.amphibians.userinterface.AmphibiansApp
+import com.example.amphibians.userinterface.AmphibiansViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: AmphibiansViewModel by viewModels {
+        val app = application as AmphibiansApplication
+        AmphibiansViewModel.provideFactory(app.appContainer.amphibiansRepository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            AmphibiansTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme {
+                AmphibiansApp(
+                    uiState = viewModel.uiState,
+                    onRetry = { viewModel.getAmphibians() }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AmphibiansTheme {
-        Greeting("Android")
     }
 }
